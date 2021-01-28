1ab4b59d7330741d097be9954d78bc151a6b7a3a
tgjag <tgjag@10.71.51.80>
2021-01-28 12:34:48 -0500

    Newest version of my program

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -4,8 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Rayan Baathim");
-		System.out.println("He has no experience with programming languages.");
+		System.out.println("Hello World!");
 	}
 
 }
