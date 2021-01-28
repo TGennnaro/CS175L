3fd172897124cfeb4b44edd6b85232c904374fdf
tgjag <tgjag@10.71.51.80>
2021-01-28 12:10:08 -0500

    My first program

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Rayan Baathim");
-		System.out.println("He has no experience with programming languages.");
+		System.out.println("He has no experience with programming languages at all.");
 	}
 
 }
