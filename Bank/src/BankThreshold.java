16d7dc33c4498fa0751e05deac7a51d105fae36c
tgjag <tgjag@10.71.48.15>
2021-04-01 00:44:59 -0400

    Bank Account V3 - Added Interest Threshold calculation

--- Bank/src/BankThreshold.java
+++ Bank/src/BankThreshold.java
@@ -1,13 +1,13 @@
 
 public class BankThreshold {
 	public int calcThreshold(double threshold, double interest, double bal){
-		int month = 0;
+		int months = 0;
 		
 		while (bal < threshold) {
 			bal = bal*(interest+1);
-			month++;
+			months++;
 		}
 		
-		return month;
+		return months;
 	}
 }
