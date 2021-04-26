7739b9eb295022ca85c352af488d41fc6c31f4c1
tgjag <tgjag@10.71.48.82>
2021-04-26 10:27:51 -0400

    New Version of Bank Account which supports multiple bank accounts

--- Bank/src/BankAccount.java
+++ Bank/src/BankAccount.java
@@ -1,53 +1,74 @@
 /**
-   A bank account has a balance and a mechanism for
-   applying interest or fees at the end of the month.
-*/
-public class BankAccount
+ * A bank account has a balance that can be changed by
+ * deposits and withdrawals
+ */
+public class BankAccount 
 {
-   private double balance;
-   private double interestPct;
+	private String name;
+	private double balance;
+	private int acct;
+	private static int accountNumber = 0;
 
-   /**
-      Constructs a bank account with zero balance.
-   */
-   public BankAccount(double amount, double interest)
-   {
-      balance = amount;
-      interestPct=interest;
-   }
-
-   /**
-      Makes a deposit into this account.
-      @param amount the amount of the deposit
-   */
-   public void deposit(double amount)
-   {
-      balance = balance + amount;
-   }
-
-   /**
-      Makes a withdrawal from this account, or charges a penalty if
-      sufficient funds are not available.
-      @param amount the amount of the withdrawal
-   */
-   public void withdraw(double amount)
-   {
-      balance = balance - amount;
-   }
-   
-   public double calcInterest() {
-	   return balance*interestPct;
-   }
-   /**
-      Gets the current balance of this bank account.
-      @return the current balance
-   */
-   public double getBalance()
-   {
-      return balance;
-   }
-   
-   public double getInterest() {
-	   return interestPct;
-   }
+	/**
+	 * Constructs a bank account with a zero balance
+	 */
+	public BankAccount()
+	{
+		balance = 0;
+	}
+	/**
+	 * Constructs a bank account with a given balance
+	 * @param initialBalance the initial balance
+	 */
+	public BankAccount(String name,double initialBalance)
+	{
+		this.balance = initialBalance;
+		this.name = name;
+		accountNumber++;
+		this.acct = accountNumber;
+		
+	}
+	/**
+	 * Deposits money into the bank account
+	 * @param amount the amount to deposit
+	 */
+	public void deposit(double amount)
+	{
+		balance = balance + amount;
+	}
+	/**
+	 * Withdraws money from the bank account
+	 * @param amount the amount to withdraw
+	 */
+	public void withdraw(double amount)
+	{
+		if (amount>balance) 
+		{
+			throw new IllegalArgumentException("WD amount "+amount+" exceeds balance of "+balance);
+		}
+		balance = balance - amount;
+	}
+	/**
+	 * Gets the current balance of the bank account
+	 * @return the current balance
+	 */
+	public double getBalance()
+	{
+		return balance;
+	}
+	public String getAccount()
+	{
+		return this.name+" " + this.acct+" " + this.balance;
+	}
+	public double anyAccount(BankAccount otherAccount) 
+	{
+		return otherAccount.getBalance();
+	}
+	public void transfer(double amount,BankAccount otherAccount) 
+	{
+		balance = balance - amount;
+		otherAccount.deposit(amount);
+		
+	}
 }
+
--- Bank/src/BankAccountTester.java
+++ Bank/src/BankAccountTester.java
@@ -1,117 +1,66 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BankAccountTester {
 
 	public static void main(String[] args) {
-		// Scanner definition
-		Scanner in = new Scanner(System.in);
+		// Initialize the initial bank accounts
+		BankAccount gil = new BankAccount("Gil", 500);
+		BankAccount joe = new BankAccount("Joe", 1000);
+		BankAccount fred = new BankAccount("Fred", 2000);
+		BankAccount sally = new BankAccount("Sally", 2500);
 		
-		// Introduction
-		System.out.print("Hello, welcome to Monmouth Bank.\nWould you like to open a Bank Account? (Y/N): ");
-		// Response from user
-		String confirm = in.nextLine().toUpperCase();
-		// Check if response was N, or No
-		if (confirm.equals("N")) {
-			System.out.println("Okay, have a great day!");
-			// Close scanner and terminate method
-			in.close();
-			return;
-		// Since response is not N, check if response is anything but Y, or Yes.
-		} else if (!confirm.equals("Y")){
-			System.out.println("Unknown Input \""+confirm+ "\". Expected: \"Y\" or \"N\"");
-			// Close scanner and terminate method
-			in.close();
-			return;
+		// Create an array list
+		ArrayList<BankAccount> accountList = new ArrayList<>();
+		// Create a List to store all the Bank Accounts in one value, makes adding to the array list much easier
+		List<BankAccount> tempList = Arrays.asList(gil, joe, fred);
+		// Add the list to the array list in 2 steps instead of 3
+		accountList.addAll(tempList);
+		
+		// Loop over all bank accounts in the list
+		for (BankAccount account : accountList) {
+			String info = account.getAccount(); // Get the account info
+			String[] split = info.split(" "); // Split it at the spaces
+			String name = split[0];
+			int acctNum = Integer.parseInt(split[1]);
+			double bal = Double.parseDouble(split[2]);
+			System.out.println(name+"'s Account Info:\nAccount Number: "+acctNum+" | Balance: $"+bal+"\n");
 		}
 		
-		// The following code only runs if the user inputed Y.
+		accountList.add(sally);
 		
-		// Ask for initial deposit
-		System.out.print("How much would you like your initial deposit to be? $");
-		int startBal = in.nextInt();
-		
-		System.out.print("What interest rate are you requesting? (Must be approved by bank): ");
-		double interest = in.nextDouble();
-		// Create object
-		BankAccount myBankAccount = new BankAccount(startBal, interest);
-		
-		// Confirm account opening
-		System.out.println("Your account has been opened with a balance of $"+startBal+".");
-		
-		// Create variable to control the loop
-		boolean running = true;
-		
-		while (running) {
-			// Display available commands
-			System.out.print("\nInput a command: Withdraw | Deposit | Balance | Interest | Threshold | Stop | Help: ");
-			// Get the inputed command
-			String command = in.next().toLowerCase();
-			
-			// If "withdraw" was inputed,
-			if (command.equals("withdraw")) {
-				// Get amount to withdraw
-				System.out.print("Please input the amount you would like to withdraw: $");
-				double amt = in.nextDouble();
-				
-				// Check to make sure they have enough funds to withdraw amount
-				if (amt <= myBankAccount.getBalance()) {
-					// If yes, withdraw
-					myBankAccount.withdraw(amt);
-					System.out.printf("Withdrew $%.2f. New balance: $%.2f", amt, myBankAccount.getBalance());
-				} else {
-					// If no, throw error
-					System.out.printf("Invalid funds to withdraw $"+amt+". Available: $%.2f",myBankAccount.getBalance());
-				}
-			// If "deposit" was inputed,
-			} else if (command.equals("deposit")) {
-				// Get amount to deposit
-				System.out.print("Please input the amount you would like to deposit: $");
-				double amt = in.nextDouble();
-				// Deposit into account
-				myBankAccount.deposit(amt);
-				System.out.printf("Deposited $%.2f. New balance: $%.2f", amt, myBankAccount.getBalance());
-			// If "balance" was inputed,
-			} else if (command.equals("balance")) {
-				// Display balance
-				System.out.printf("Your current balance is $%.2f", myBankAccount.getBalance());
-			} else if (command.equals("interest")) {
-				System.out.printf("Your current accumulated interest is $%.2f at a rate of "+myBankAccount.getInterest()+".", myBankAccount.calcInterest());
-			// If "threshold" was inputed
-			} else if (command.equals("threshold")) { // ***** NEW SECTION ADDED *****
-				BankThreshold threshold = new BankThreshold();
-				boolean loop = true;
-				while (loop) {
-					System.out.print("Enter the interest threshold (Enter any letter to end): ");
-					if (in.hasNextDouble()) {
-						double th = in.nextDouble(); // Threshold was already taken, so I used th.
-						int months = threshold.calcThreshold(th, interest, myBankAccount.getBalance());
-						System.out.println("It will take "+months+" months to reach your threshold.");
-					} else {
-						@SuppressWarnings("unused") // To get rid of the annoying warnings for the variable...
-						String dummy = in.next(); // To prevent original loop from firing again.
-						loop = false;
-					}
-				} // ***** END OF NEW SECTION *****
-			// If "stop" was inputed
-			} else if (command.equals("stop")) {
-				System.out.println("Thank you for your business, have a great day!");
-				// Stop the loop
-				running = false;
-			// If "help" was inputed,
-			} else if (command.equals("help")) {
-				// Display all commands and their functions
-				System.out.println("\nHelp: ");
-				System.out.println("Withdraw: Withdraw funds from your account");
-				System.out.println("Deposit: Deposit funds from your account");
-				System.out.println("Balance: View your account balance");
-				System.out.println("Stop: Exit the bank menu");
-			// If anything but the valid commands are inputed,
-			} else {
-				// Throw error
-				System.out.println("Invalid input: \""+command+"\".");
+		double highestBal = accountList.get(0).getBalance(); // Initialize highest as the first balance in the list
+		int highestAcctNum = 1; // Initialize highest account number as the first account number in the list
+		String highestName = "";  // Initialize the highest name
+		for (BankAccount account : accountList) { // Loop over all accounts
+			if (account.getBalance() > highestBal) { // If their balance > the current highest
+				String info = account.getAccount(); // Set all the highest info to the new account's info
+				String[] split = info.split(" ");
+				highestName = split[0];
+				highestAcctNum = Integer.parseInt(split[1]);
+				highestBal = Double.parseDouble(split[2]);
 			}
 		}
+		System.out.println("Highest Account Balance:");
+		System.out.println(highestName+"'s Account Info:\nAccount Number: "+highestAcctNum+" | Balance: $"+highestBal+"\n"); // Print the info
 		
-		// Close scanner
-		in.close();
+		accountList.remove(sally);
+		
+		highestBal = accountList.get(0).getBalance(); // Reset the highest balance as the first balance in the list
+		highestAcctNum = 1; // Reset the highest account number as the first account number in the list
+		highestName = ""; // Reset the highest name
+		for (BankAccount account : accountList) { // Loop over all accounts
+			if (account.getBalance() > highestBal) { // If their balance > the current highest
+				String info = account.getAccount(); // Set all the highest info to the new account's info
+				String[] split = info.split(" ");
+				highestName = split[0];
+				highestAcctNum = Integer.parseInt(split[1]);
+				highestBal = Double.parseDouble(split[2]);
+			}
+		}
+		System.out.println("New Highest Account Balance:");
+		System.out.println(highestName+"'s Account Info:\nAccount Number: "+highestAcctNum+" | Balance: $"+highestBal+"\n"); // Print the info
 	}
+
 }
